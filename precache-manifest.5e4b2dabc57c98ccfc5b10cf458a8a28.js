aa55f6f826324ee077049fe560c9cff8447e5d66
Sean Zhou <sean.sy.zhou@gmail.com>
2020-09-08 00:32:23 +0800

    Updates

---
+++ precache-manifest.5e4b2dabc57c98ccfc5b10cf458a8a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1fa62e3396d34e5c126c8d9a3bbd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27926a4da512a763a0d9",
+    "url": "/static/css/main.c0e66084.chunk.css"
+  },
+  {
+    "revision": "062ba46dce852f809ef0",
+    "url": "/static/js/2.af047551.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.af047551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27926a4da512a763a0d9",
+    "url": "/static/js/main.7cfbdfa9.chunk.js"
+  },
+  {
+    "revision": "3923032140779a050844",
+    "url": "/static/js/runtime-main.f7e87a33.js"
+  }
+]);
